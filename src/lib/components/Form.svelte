c8b9a9492268219da56ab8851a7923404ecd67e3
Hieracosphynx <cortezmiguel05@gmail.com>
2024-08-11 20:44:06 -0700

    WIP: Form

---
+++ src/lib/components/Form.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { enhance } from "$app/forms";
+
+let gameFranchise = "";
+let gameName = "";
+
+</script>
+
+<div>
+    <div class="flex-1 flex-col">
+        <div class="flex-1 flex-col">
+            <p>Game Franchise:</p>
+            <input class="rounded border-red-800 border-2" type="text" bind:value={gameFranchise}/>
+        </div>
+        <div class="flex-1 flex-col">
+            <p>Game Name:</p>
+            <input class="rounded border-red-800 border-2" type="text" bind:value={gameName} />
+        </div>
+    </div>
+
+    <form method="POST" use:enhance>
+        <button on:click|preventDefault={()=>{ console.log("TOLD TO") }}>SUBMIT</button>
+    </form>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Form from "$lib/components/Form.svelte";
+</script>
+
+<Form />
